3661829c86b7d54a66bfb393c7ed25867d2ab6d6
Victor <2bravepickle@gmail.com>
2018-12-16 23:00:41 +0200

    fixing tests

--- tests/Channel/HttpChannelTest.php
+++ tests/Channel/HttpChannelTest.php
@@ -111,6 +111,7 @@ class HttpChannelTest extends TestCase
                     'request_options' => [
                         'handler' => $handler,
                         'debug' => true,
+                        'allow_redirects' => false,
                     ],
                 ]
             ]
@@ -118,10 +119,6 @@ class HttpChannelTest extends TestCase
 
         $channel = new HttpChannel($configManager);
         $actualResponses = $channel->send($input);
-
-//        print_r($expected);
-//        print_r($actualResponses);
-//        print_r(array_keys($actualResponses));
 
         $this->assertFalse($channel->hasErrors(), 'Errors found in processes.');
 
@@ -148,16 +145,16 @@ class HttpChannelTest extends TestCase
         $this->assertEquals($expected[1]['headers'], $actualResponse->getHeaders(), 'Headers differ');
         $this->assertEquals($expected[1]['data'], $actualResponse->getBody()->getContents(), 'Body differs');
 
-//        $this->assertArrayHasKey($expected[2]['id'], $actualResponses);
-//
-//        /** @var Response $actualResponse */
-//        $actualResponse = $actualResponses[$expected[2]['id']];
-//
-//        $this->assertTrue($actualResponse instanceof ResponseInterface, 'Response should be PSR-7 interface.');
-//
-//        $this->assertEquals($expected[2]['statusCode'], $actualResponse->getStatusCode(), 'Status code differ');
-//        $this->assertEquals($expected[2]['headers'], $actualResponse->getHeaders(), 'Headers differ');
-//        $this->assertEquals($expected[2]['data'], $actualResponse->getBody()->getContents(), 'Body differs');
+        $this->assertArrayHasKey($expected[2]['id'], $actualResponses);
+
+        /** @var Response $actualResponse */
+        $actualResponse = $actualResponses[$expected[2]['id']];
+
+        $this->assertTrue($actualResponse instanceof ResponseInterface, 'Response should be PSR-7 interface.');
+
+        $this->assertEquals($expected[2]['statusCode'], $actualResponse->getStatusCode(), 'Status code differ');
+        $this->assertEquals($expected[2]['headers'], $actualResponse->getHeaders(), 'Headers differ');
+        $this->assertEquals($expected[2]['data'], $actualResponse->getBody()->getContents(), 'Body differs');
     }
 
     /**
@@ -168,16 +165,16 @@ class HttpChannelTest extends TestCase
         $queues = [
             [
                 (new Request())
-                ->setId('post_comments')
-                ->setUri('http://example.com/comments')
-                ->setData([
-                    'text' => 'Nice job!',
-                ]),
+                    ->setId('post_comments')
+                    ->setUri('http://example.com/comments')
+                    ->setData([
+                        'text' => 'Nice job!',
+                    ]),
 
-//                (new Request())
-//                    ->setId('head_comments')
-//                    ->setUri('http://example.com/comments/1')
-//                    ->setMethod('HEAD')
+                (new Request())
+                    ->setId('head_comments')
+                    ->setUri('http://example.com/comments/1')
+                    ->setMethod('HEAD')
             ],
             [(new Request())
                 ->setMethod('GET')
@@ -203,22 +200,99 @@ class HttpChannelTest extends TestCase
                 'headers' => ['Content-Type' => ['application/json']],
                 'data' => '{"success": true}',
             ],
-//            [
-//                'id' => 'head_comments',
-//                'statusCode' => 301,
-//                'headers' => ['Location' => ['http://www.example.com/comments/1']],
-//                'data' => '',
-//            ],
+            [
+                'id' => 'head_comments',
+                'statusCode' => 301,
+                'headers' => ['Location' => ['http://www.example.com/comments/1']],
+                'data' => '',
+            ],
         ];
 
         $responses = [
             new Response($expected[0]['statusCode'], $expected[0]['headers'], stream_for($expected[0]['data'])),
             new Response($expected[1]['statusCode'], $expected[1]['headers'], stream_for($expected[1]['data'])),
-//            new Response($expected[2]['statusCode'], $expected[2]['headers'], stream_for($expected[2]['data'])),
+            new Response($expected[2]['statusCode'], $expected[2]['headers'], stream_for($expected[2]['data'])),
         ];
 
         return [
             [$queues, $responses, $expected],
         ];
+    }
+
+    public function testConcurrency()
+    {
+        $handler = HandlerStack::create(new MockHandler([
+            new Response(200, [], stream_for('first')),
+            new Response(201, [], stream_for('second')),
+            new Response(202, [], stream_for('third!')),
+        ]));
+
+        $configManager = new ConfigManager();
+        $configManager->addConfiguration([
+            'domains' => ['http://example.com',],
+            'channels' => [
+                'http' => [
+                    'request_options' => [
+                        'handler' => $handler,
+                        'debug' => true,
+                    ],
+                ]
+            ]
+        ]);
+
+        $input = [
+            [
+                (new Request())
+                    ->setId('post_comments')
+                    ->setUri('http://example.com/comments'),
+
+                (new Request())
+                    ->setId('head_comments')
+                    ->setUri('http://example.com/comments/1')
+                    ->setMethod('HEAD')
+            ],
+            [(new Request())
+                ->setMethod('GET')
+                ->setUri('http://example.com/users')
+            ],
+        ];
+
+
+        $channel = new HttpChannel($configManager);
+        $actualResponses = $channel->send($input);
+
+//        print_r($expected);
+//        print_r($actualResponses);
+        print_r(array_keys($actualResponses));
+
+        die("\n" . __METHOD__ . ":" . __FILE__ . ":" . __LINE__ . "\n");
+
+        $this->assertNotEmpty($actualResponses);
+        $this->assertTrue(count($actualResponses) == 3);
+//        $this->assertFalse($channel->hasErrors(), 'Errors found in processes.');
+//
+//        $this->assertArrayHasKey($expected[0]['id'], $actualResponses);
+//
+//        /** @var Response $actualResponse */
+//        $actualResponse = $actualResponses[$expected[0]['id']];
+//
+//        $this->assertTrue($actualResponse instanceof ResponseInterface, 'Response should be PSR-7 interface.');
+//
+//
+//        $this->assertEquals($expected[0]['statusCode'], $actualResponse->getStatusCode(), 'Status code differ');
+//        $this->assertEquals($expected[0]['headers'], $actualResponse->getHeaders(), 'Headers differ');
+//        $this->assertEquals($expected[0]['data'], $actualResponse->getBody()->getContents(), 'Body differs');
+//
+//        $this->assertArrayHasKey($expected[1]['id'], $actualResponses);
+//
+//        /** @var Response $actualResponse */
+//        $actualResponse = $actualResponses[$expected[1]['id']];
+//
+//        $this->assertTrue($actualResponse instanceof ResponseInterface, 'Response should be PSR-7 interface.');
+//
+//        $this->assertEquals($expected[1]['statusCode'], $actualResponse->getStatusCode(), 'Status code differ');
+//        $this->assertEquals($expected[1]['headers'], $actualResponse->getHeaders(), 'Headers differ');
+//        $this->assertEquals($expected[1]['data'], $actualResponse->getBody()->getContents(), 'Body differs');
+
     }
 }
