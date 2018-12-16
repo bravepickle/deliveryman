9178ca595e00faf749ebefb9bd849d379207d116
Victor <2bravepickle@gmail.com>
2018-12-16 23:05:13 +0200

    add test single queue

--- tests/Channel/HttpChannelTest.php
+++ tests/Channel/HttpChannelTest.php
@@ -219,4 +219,134 @@ class HttpChannelTest extends TestCase
         ];
     }
 
+
+    /**
+     * @dataProvider singleQueue
+     * @param array $input
+     * @param $returnResponses
+     * @param array $expected
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Psr\Cache\InvalidArgumentException
+     * @throws \Deliveryman\Exception\ChannelException
+     */
+    public function testSingleQueue(array $input, array $returnResponses, array $expected)
+    {
+        $handler = HandlerStack::create(new MockHandler($returnResponses));
+
+        $configManager = new ConfigManager();
+        $configManager->addConfiguration([
+            'domains' => ['http://example.com',],
+            'channels' => [
+                'http' => [
+                    'request_options' => [
+                        'handler' => $handler,
+                        'debug' => true,
+                        'allow_redirects' => false,
+                    ],
+                ]
+            ]
+        ]);
+
+        $channel = new HttpChannel($configManager);
+        $actualResponses = $channel->send($input);
+
+        $this->assertFalse($channel->hasErrors(), 'Errors found in processes.');
+
+        $this->assertArrayHasKey($expected[0]['id'], $actualResponses);
+
+        /** @var Response $actualResponse */
+        $actualResponse = $actualResponses[$expected[0]['id']];
+
+        $this->assertTrue($actualResponse instanceof ResponseInterface, 'Response should be PSR-7 interface.');
+
+
+        $this->assertEquals($expected[0]['statusCode'], $actualResponse->getStatusCode(), 'Status code differ');
+        $this->assertEquals($expected[0]['headers'], $actualResponse->getHeaders(), 'Headers differ');
+        $this->assertEquals($expected[0]['data'], $actualResponse->getBody()->getContents(), 'Body differs');
+
+        $this->assertArrayHasKey($expected[1]['id'], $actualResponses);
+
+        /** @var Response $actualResponse */
+        $actualResponse = $actualResponses[$expected[1]['id']];
+
+        $this->assertTrue($actualResponse instanceof ResponseInterface, 'Response should be PSR-7 interface.');
+
+        $this->assertEquals($expected[1]['statusCode'], $actualResponse->getStatusCode(), 'Status code differ');
+        $this->assertEquals($expected[1]['headers'], $actualResponse->getHeaders(), 'Headers differ');
+        $this->assertEquals($expected[1]['data'], $actualResponse->getBody()->getContents(), 'Body differs');
+
+        $this->assertArrayHasKey($expected[2]['id'], $actualResponses);
+
+        /** @var Response $actualResponse */
+        $actualResponse = $actualResponses[$expected[2]['id']];
+
+        $this->assertTrue($actualResponse instanceof ResponseInterface, 'Response should be PSR-7 interface.');
+
+        $this->assertEquals($expected[2]['statusCode'], $actualResponse->getStatusCode(), 'Status code differ');
+        $this->assertEquals($expected[2]['headers'], $actualResponse->getHeaders(), 'Headers differ');
+        $this->assertEquals($expected[2]['data'], $actualResponse->getBody()->getContents(), 'Body differs');
+    }
+
+    /**
+     * @return array
+     */
+    public function singleQueue()
+    {
+        $queues = [
+            [
+                (new Request())
+                    ->setId('post_comments')
+                    ->setUri('http://example.com/comments')
+                    ->setData([
+                        'text' => 'Nice job!',
+                    ]),
+
+                (new Request())
+                    ->setMethod('GET')
+                    ->setUri('http://example.com/users/2')
+                    ->setQuery(['uid' => 'zest'])
+                    ->setHeaders([
+                        (new RequestHeader())->setName('X-API-Key')->setValue('test1'),
+                        (new RequestHeader())->setName('X-API-Key')->setValue('test2')
+                    ]),
+
+                (new Request())
+                    ->setId('head_comments')
+                    ->setUri('http://example.com/comments/1')
+                    ->setMethod('HEAD')
+            ],
+        ];
+
+        $expected = [
+            [
+                'id' => 'post_comments',
+                'statusCode' => 400,
+                'headers' => ['Content-Type' => ['plain/text; utf8']],
+                'data' => 'Invalid input format',
+            ],
+            [
+                'id' => 'GET_http://example.com/users/2',
+                'statusCode' => 200,
+                'headers' => ['Content-Type' => ['application/json']],
+                'data' => '{"success": true}',
+            ],
+            [
+                'id' => 'head_comments',
+                'statusCode' => 301,
+                'headers' => ['Location' => ['http://www.example.com/comments/1']],
+                'data' => '',
+            ],
+        ];
+
+        $responses = [
+            new Response($expected[0]['statusCode'], $expected[0]['headers'], stream_for($expected[0]['data'])),
+            new Response($expected[1]['statusCode'], $expected[1]['headers'], stream_for($expected[1]['data'])),
+            new Response($expected[2]['statusCode'], $expected[2]['headers'], stream_for($expected[2]['data'])),
+        ];
+
+        return [
+            [$queues, $responses, $expected],
+        ];
+    }
+
 }
